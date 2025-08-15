2033ff60944e7d7e053330463dd08f3baf69d87e
Petra Jaros <peeja@peeja.com>
2025-08-15 12:22:01 -0400

    refactor: `executeDAGScan` doesn't need to be exported

--- pkg/preparation/dags/dags.go
+++ pkg/preparation/dags/dags.go
@@ -75,7 +75,7 @@ func (a API) RunDagScansForUpload(ctx context.Context, uploadID id.UploadID, nod
 			switch dagScan.State() {
 			case model.DAGScanStatePending:
 				log.Debugf("Executing dag scan %s in state %s", dagScan.FsEntryID(), dagScan.State())
-				if err := a.ExecuteDAGScan(ctx, dagScan, nodeCB); err != nil {
+				if err := a.executeDAGScan(ctx, dagScan, nodeCB); err != nil {
 					return fmt.Errorf("executing dag scan %s: %w", dagScan.FsEntryID(), err)
 				}
 				executions++
@@ -86,7 +86,7 @@ func (a API) RunDagScansForUpload(ctx context.Context, uploadID id.UploadID, nod
 				}
 				// if the scan is now in a state where it can be executed, execute it
 				if dagScan.State() == model.DAGScanStatePending {
-					if err := a.ExecuteDAGScan(ctx, dagScan, nodeCB); err != nil {
+					if err := a.executeDAGScan(ctx, dagScan, nodeCB); err != nil {
 						return fmt.Errorf("executing dag scan %s after handling awaiting children: %w", dagScan.FsEntryID(), err)
 					}
 					executions++
@@ -101,8 +101,8 @@ func (a API) RunDagScansForUpload(ctx context.Context, uploadID id.UploadID, nod
 	}
 }
 
-// ExecuteDAGScan executes a dag scan on the given fs entry, creating a unix fs dag for the given file or directory.
-func (a API) ExecuteDAGScan(ctx context.Context, dagScan model.DAGScan, nodeCB func(node model.Node, data []byte) error) error {
+// executeDAGScan executes a dag scan on the given fs entry, creating a unix fs dag for the given file or directory.
+func (a API) executeDAGScan(ctx context.Context, dagScan model.DAGScan, nodeCB func(node model.Node, data []byte) error) error {
 	err := dagScan.Start()
 	if err != nil {
 		log.Debug("Failed to start dag scan:", err)
@@ -112,7 +112,17 @@ func (a API) ExecuteDAGScan(ctx context.Context, dagScan model.DAGScan, nodeCB f
 		log.Debugf("Failed to update dag scan %s: %v", dagScan.FsEntryID(), err)
 		return fmt.Errorf("updating dag scan: %w", err)
 	}
-	cid, err := a.executeDAGScan(ctx, dagScan, nodeCB)
+
+	var cid cid.Cid
+	switch ds := dagScan.(type) {
+	case *model.FileDAGScan:
+		cid, err = a.executeFileDAGScan(ctx, ds, nodeCB)
+	case *model.DirectoryDAGScan:
+		cid, err = a.executeDirectoryDAGScan(ctx, ds, nodeCB)
+	default:
+		return fmt.Errorf("unrecognized DAG scan type: %T", dagScan)
+	}
+
 	if err != nil {
 		if errors.Is(err, context.Canceled) {
 			if err := dagScan.Cancel(); err != nil {
@@ -135,17 +145,6 @@ func (a API) ExecuteDAGScan(ctx context.Context, dagScan model.DAGScan, nodeCB f
 		return fmt.Errorf("updating dag scan after fail: %w", err)
 	}
 	return nil
-}
-
-func (a API) executeDAGScan(ctx context.Context, dagScan model.DAGScan, nodeCB func(node model.Node, data []byte) error) (cid.Cid, error) {
-	switch ds := dagScan.(type) {
-	case *model.FileDAGScan:
-		return a.executeFileDAGScan(ctx, ds, nodeCB)
-	case *model.DirectoryDAGScan:
-		return a.executeDirectoryDAGScan(ctx, ds, nodeCB)
-	default:
-		return cid.Undef, fmt.Errorf("unrecognized DAG scan type: %T", dagScan)
-	}
 }
 
 func (a API) executeFileDAGScan(ctx context.Context, dagScan *model.FileDAGScan, nodeCB func(node model.Node, data []byte) error) (cid.Cid, error) {
