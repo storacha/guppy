993d9b142724cf3af03a9c51949c308540e026a0
Petra Jaros <peeja@peeja.com>
2025-08-25 11:17:27 -0400

    refactor: Separate out `storacha` API

---
+++ pkg/preparation/storacha/repo.go
@@ -0,0 +1,14 @@
+package storacha
+
+import (
+	"context"
+
+	"github.com/storacha/guppy/pkg/preparation/shards/model"
+	"github.com/storacha/guppy/pkg/preparation/types/id"
+)
+
+// Repo defines the interface for interacting with DAG scans, nodes, and links in the repository.
+type Repo interface {
+	UpdateShard(ctx context.Context, shard *model.Shard) error
+	ShardsForUploadByStatus(ctx context.Context, uploadID id.UploadID, state model.ShardState) ([]*model.Shard, error)
+}
