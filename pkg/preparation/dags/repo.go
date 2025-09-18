b08a8b118e0453ca9d5ee7b523afa065705fbdc4
Petra Jaros <peeja@peeja.com>
2025-09-18 14:23:31 -0400

    refactor: Remove `DAGScanStateAwaitingChildren`
    We don't actually need it. It's almost exactly the same thing as the
    pending state for a directory. The only time it makes a difference is if
    the process is interrupted after the directory notices the children are
    completed but before completing itself, and the consequence is an
    additional check in the DB. The check is also cheaper now, as it's a
    single query.

--- pkg/preparation/dags/repo.go
+++ pkg/preparation/dags/repo.go
@@ -16,7 +16,7 @@ type Repo interface {
 	FindOrCreateUnixFSNode(ctx context.Context, cid cid.Cid, size uint64, spaceDID did.DID, ufsdata []byte) (*model.UnixFSNode, bool, error)
 	CreateLinks(ctx context.Context, parent cid.Cid, spaceDID did.DID, links []model.LinkParams) error
 	LinksForCID(ctx context.Context, cid cid.Cid, spaceDID did.DID) ([]*model.Link, error)
-	GetChildScans(ctx context.Context, directoryScans *model.DirectoryDAGScan) ([]model.DAGScan, error)
+	HasIncompleteChildren(ctx context.Context, directoryScans *model.DirectoryDAGScan) (bool, error)
 	DAGScansForUploadByStatus(ctx context.Context, uploadID id.UploadID, states ...model.DAGScanState) ([]model.DAGScan, error)
 	DirectoryLinks(ctx context.Context, dirScan *model.DirectoryDAGScan) ([]model.LinkParams, error)
 	DeleteNodes(ctx context.Context, spaceDID did.DID, nodeCIDs []cid.Cid) error
