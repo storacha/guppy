e83f9a9b9a2161e999df04e248830f997be37f22
Petra Jaros <peeja@peeja.com>
2025-10-08 15:35:24 -0400

    feat: CLI traces a single span with OpenTelemetry

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "github.com/storacha/guppy/cmd"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/storacha/guppy/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	var err error
+	ctx := context.Background()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
+	defer func() {
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	cmd.ExecuteContext(ctx)
 }
