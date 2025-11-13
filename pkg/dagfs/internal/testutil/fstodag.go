b88a8b83a5553389b2a7577abeb6b3682be7b320
Petra Jaros <peeja@peeja.com>
2025-11-13 18:40:34 -0500

    feat: DAG-backed `fs.FS` implementation

---
+++ pkg/dagfs/internal/testutil/fstodag.go
@@ -0,0 +1,100 @@
+package testutil
+
+import (
+	"context"
+	"io/fs"
+	"path"
+
+	chunker "github.com/ipfs/boxo/chunker"
+	"github.com/ipfs/boxo/ipld/unixfs/importer"
+	uio "github.com/ipfs/boxo/ipld/unixfs/io"
+	ipld "github.com/ipfs/go-ipld-format"
+)
+
+// BuildDAGFromFS creates a UnixFS DAG from an fs.FS filesystem.
+// It recursively walks the filesystem, creating directory nodes and file nodes.
+// Returns the root node of the created DAG.
+//
+// Example usage:
+//
+//	ds := mdtest.Mock()
+//	fsys := afero.NewIOFS(memFS)
+//	rootNode, err := BuildDAGFromFS(context.Background(), ds, fsys)
+func BuildDAGFromFS(ctx context.Context, dagService ipld.DAGService, fsys fs.FS) (ipld.Node, error) {
+	return buildDAGFromDir(ctx, dagService, fsys, ".")
+}
+
+// buildDAGFromDir recursively builds a DAG from a directory in the filesystem
+func buildDAGFromDir(ctx context.Context, dagService ipld.DAGService, fsys fs.FS, dirPath string) (ipld.Node, error) {
+	// Create a new directory
+	dir, err := uio.NewDirectory(dagService)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read directory entries
+	entries, err := fs.ReadDir(fsys, dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Process each entry
+	for _, entry := range entries {
+		entryPath := path.Join(dirPath, entry.Name())
+		if dirPath == "." {
+			entryPath = entry.Name()
+		}
+
+		var childNode ipld.Node
+
+		if entry.IsDir() {
+			// Recursively build subdirectory
+			childNode, err = buildDAGFromDir(ctx, dagService, fsys, entryPath)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Build file node
+			childNode, err = buildDAGFromFile(ctx, dagService, fsys, entryPath)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		// Add child to directory
+		err = dir.AddChild(ctx, entry.Name(), childNode)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Get the directory node
+	node, err := dir.GetNode()
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the directory node to the DAG service
+	err = dagService.Add(ctx, node)
+	if err != nil {
+		return nil, err
+	}
+
+	return node, nil
+}
+
+// buildDAGFromFile creates a UnixFS file node from a file in the filesystem
+func buildDAGFromFile(ctx context.Context, dagService ipld.DAGService, fsys fs.FS, filePath string) (ipld.Node, error) {
+	// Open the file
+	file, err := fsys.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a splitter from the file
+	spl := chunker.DefaultSplitter(file)
+
+	// Build the DAG from the file
+	return importer.BuildDagFromReader(dagService, spl)
+}
