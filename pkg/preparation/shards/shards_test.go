cf90d3b3a803afb161406bbb8fd39d63a1579ad0
ash <alan@storacha.network>
2025-11-19 14:49:32 +0000

    Update pkg/preparation/shards/shards_test.go
    Co-authored-by: Petra Jaros <peeja@peeja.com>

--- pkg/preparation/shards/shards_test.go
+++ pkg/preparation/shards/shards_test.go
@@ -395,8 +395,6 @@ func TestIndexForUpload(t *testing.T) {
 		require.NoError(t, err)
 		err = repo.UpdateShard(t.Context(), shard3)
 		require.NoError(t, err)
-		err = shard3.Close()
-		require.NoError(t, err)
 
 		rootLink := testutil.RandomCID(t)
 		err = upload.SetRootCID(rootLink.(cidlink.Link).Cid)
