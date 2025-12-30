ca3bbca9865ba3b6cce2e91015b3acc48015a600
hannahhoward <hannah@hannahhoward.net>
2025-12-29 16:58:33 -0800

    refactor(blobs): seperate add node to shard worker
    seperates out the process of adding nodes to shards to its own worker, by renaming and elevating
    nodes_in_shards to a node_uploads table with its own model

--- pkg/preparation/uploads/repo.go
+++ pkg/preparation/uploads/repo.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ipfs/go-cid"
 	"github.com/storacha/go-ucanto/did"
+	blobsmodel "github.com/storacha/guppy/pkg/preparation/blobs/model"
 	dagmodel "github.com/storacha/guppy/pkg/preparation/dags/model"
 	"github.com/storacha/guppy/pkg/preparation/types/id"
 	uploadmodel "github.com/storacha/guppy/pkg/preparation/uploads/model"
@@ -23,4 +24,7 @@ type Repo interface {
 	CreateDAGScan(ctx context.Context, fsEntryID id.FSEntryID, isDirectory bool, uploadID id.UploadID, spaceDID did.DID) (dagmodel.DAGScan, error)
 	// ListSpaceSources lists all space sources for the given space DID.
 	ListSpaceSources(ctx context.Context, spaceDID did.DID) ([]id.SourceID, error)
+	// FindOrCreateNodeUpload finds or creates a node upload record.
+	// Returns (nodeUpload, created, error) where created=true if a new record was inserted.
+	FindOrCreateNodeUpload(ctx context.Context, uploadID id.UploadID, nodeCID cid.Cid, spaceDID did.DID) (*blobsmodel.NodeUpload, bool, error)
 }
