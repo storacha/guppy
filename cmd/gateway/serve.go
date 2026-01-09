f65d3aee6a31dd760002697647b4e63888141eb2
Alan Shaw <alan@storacha.network>
2026-01-09 13:35:36 +0000

    feat: size bounded cache and polish

--- cmd/gateway/serve.go
+++ cmd/gateway/serve.go
@@ -1,7 +1,10 @@
 package gateway
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"path/filepath"
 	"time"
 
@@ -9,17 +12,37 @@ import (
 	"github.com/ipfs/boxo/blockstore"
 	"github.com/ipfs/boxo/gateway"
 	"github.com/ipfs/boxo/gateway/assets"
-	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/sync"
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/color"
 	"github.com/spf13/cobra"
+	arc "github.com/storacha/go-ds-arc"
 	contentcap "github.com/storacha/go-libstoracha/capabilities/space/content"
 	"github.com/storacha/go-ucanto/core/delegation"
 	"github.com/storacha/go-ucanto/did"
 	"github.com/storacha/guppy/internal/cmdutil"
+	"github.com/storacha/guppy/pkg/build"
 	"github.com/storacha/guppy/pkg/client/dagservice"
 	"github.com/storacha/guppy/pkg/client/locator"
+	"go.uber.org/zap"
 )
+
+const (
+	port = 3000
+	// blockCacheCapacity defines the number of blocks to cache in memory. Blocks
+	// are typically <1MB due to IPFS chunking, so an upper bound for how much
+	// memory the cache will utilize is approximately [blockCacheCapacity] MB.
+	// e.g. capacity for 1,000 blocks ~= 1GB of memory.
+	blockCacheCapacity = 1000
+)
+
+var log = logging.Logger("cmd/gateway")
+
+var serveFlags struct {
+	port               int
+	blockCacheCapacity int
+}
 
 var serveCmd = &cobra.Command{
 	Use:   "serve <space-did>",
@@ -57,7 +80,8 @@ var serveCmd = &cobra.Command{
 		exchange := dagservice.NewExchange(locator, client, space)
 
 		gwConf := gateway.Config{
-			NoDNSLink: true,
+			DeserializedResponses: true,
+			NoDNSLink:             true,
 			Menu: []assets.MenuItem{
 				{
 					Title: "Storacha Network",
@@ -66,8 +90,7 @@ var serveCmd = &cobra.Command{
 			},
 		}
 
-		// TODO: what is this for? is it a cache, does it need to expire?
-		blockStore := blockstore.NewBlockstore(sync.MutexWrap(datastore.NewMapDatastore()))
+		blockStore := blockstore.NewBlockstore(arc.New(serveFlags.blockCacheCapacity))
 		blockService := blockservice.New(blockStore, exchange)
 
 		backend, err := gateway.NewBlocksBackend(blockService)
@@ -75,20 +98,106 @@ var serveCmd = &cobra.Command{
 
 		ipfsHandler := gateway.NewHandler(gwConf, backend)
 
+		e := echo.New()
+		e.HideBanner = true
+		e.HidePort = true
+
+		e.Use(requestLogger(log))
+		e.Use(middleware.Recover())
+
+		e.GET("/ipfs/*", echo.WrapHandler(ipfsHandler))
+
+		// print banner after short delay to ensure it only appears if no errors
+		// occurred during startup
 		timer := time.NewTimer(time.Second)
 		defer timer.Stop()
 		go func() {
 			<-timer.C
-			cmd.Printf("Gateway is running at http://localhost:%s", "3000")
+			cmd.Println(banner(build.Version, serveFlags.port))
 		}()
 
-		e := echo.New()
-		e.GET("/ipfs/*", echo.WrapHandler(ipfsHandler))
-		e.Logger.Fatal(e.Start(":3000"))
+		// shut down the server gracefully on context cancellation
+		go func() {
+			<-cmd.Context().Done()
+			cmd.Println("\nShutting down server...")
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			if err := e.Shutdown(ctx); err != nil {
+				cmd.PrintErrf("shutting down server: %s", err.Error())
+			}
+		}()
+
+		addr := fmt.Sprintf(":%d", serveFlags.port)
+		if err := e.Start(addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("closing server: %w", err)
+		}
 		return nil
 	},
 }
 
 func init() {
+	logging.SetLogLevel("cmd/gateway", "info")
+	serveCmd.Flags().IntVarP(&serveFlags.port, "port", "p", port, "Port to run the HTTP server on")
+	serveCmd.Flags().IntVarP(&serveFlags.blockCacheCapacity, "block-cache-capacity", "c", blockCacheCapacity, "Number of blocks to cache in memory")
 	GatewayCmd.AddCommand(serveCmd)
+}
+
+func banner(version string, port int) string {
+	return fmt.Sprintf(
+		`
+▄▖           
+▌ ▌▌▛▌▌▌▌▀▌▌▌
+▙▌▙▌▙▌▚▚▘█▌▙▌
+    ▌      ▄▌ %s
+
+High performance IPFS Gateway
+%s
+-----------------------------
+⇨ HTTP server started on %s`,
+		color.Red(version),
+		color.Blue("https://storacha.network"),
+		color.Green(fmt.Sprintf("http://localhost:%d", port)),
+	)
+}
+
+func requestLogger(logger *logging.ZapEventLogger) echo.MiddlewareFunc {
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogMethod:        true,
+		LogLatency:       true,
+		LogRemoteIP:      true,
+		LogHost:          true,
+		LogURI:           true,
+		LogUserAgent:     true,
+		LogStatus:        true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+		LogHeaders:       []string{},
+		LogError:         true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			fields := []zap.Field{
+				zap.Int("status", v.Status),
+				zap.String("method", v.Method),
+				zap.String("uri", v.URI),
+				zap.String("host", v.Host),
+				zap.String("remote_ip", v.RemoteIP),
+				zap.Duration("latency", v.Latency),
+				zap.String("user_agent", v.UserAgent),
+				zap.String("content_length", v.ContentLength),
+				zap.Int64("response_size", v.ResponseSize),
+				zap.Reflect("headers", v.Headers),
+			}
+			if v.Error != nil {
+				fields = append(fields, zap.Error(v.Error))
+			}
+			switch {
+			case v.Status >= http.StatusInternalServerError:
+				logger.WithOptions(zap.Fields(fields...)).Error("server error")
+			case v.Status >= http.StatusBadRequest:
+				logger.WithOptions(zap.Fields(fields...)).Warn("client error")
+			default:
+				logger.WithOptions(zap.Fields(fields...)).Info("request")
+			}
+			return nil
+		},
+	})
 }
--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/ipfs/boxo v0.30.0
 	github.com/ipfs/go-block-format v0.2.1
 	github.com/ipfs/go-cid v0.5.0
-	github.com/ipfs/go-datastore v0.8.2
+	github.com/ipfs/go-datastore v0.9.0
 	github.com/ipfs/go-ipld-cbor v0.2.0
 	github.com/ipfs/go-ipld-format v0.6.1
 	github.com/ipfs/go-log/v2 v2.6.0
@@ -27,7 +27,8 @@ require (
 	github.com/ipld/go-car/v2 v2.14.3
 	github.com/ipld/go-codec-dagpb v1.7.0
 	github.com/ipld/go-ipld-prime v0.21.1-0.20240917223228-6148356a4c2e
-	github.com/labstack/echo/v4 v4.13.4
+	github.com/labstack/echo/v4 v4.15.0
+	github.com/labstack/gommon v0.4.2
 	github.com/mattn/go-isatty v0.0.20
 	github.com/mitchellh/go-wordwrap v1.0.1
 	github.com/multiformats/go-multibase v0.2.0
@@ -37,6 +38,7 @@ require (
 	github.com/spf13/afero v1.12.0
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/pflag v1.0.6
+	github.com/storacha/go-ds-arc v0.0.1
 	github.com/storacha/go-libstoracha v0.6.7
 	github.com/storacha/go-ucanto v0.7.2-0.20251119232529-559b0a57d38f
 	github.com/storacha/indexing-service v1.12.2
@@ -47,7 +49,7 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.31.0
 	go.opentelemetry.io/otel/sdk v1.38.0
 	go.opentelemetry.io/otel/trace v1.38.0
-	golang.org/x/sync v0.18.0
+	golang.org/x/sync v0.19.0
 	modernc.org/sqlite v1.38.2
 )
 
@@ -71,6 +73,7 @@ require (
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/hashicorp/golang-lru/arc/v2 v2.0.7 // indirect
 	github.com/ipfs/go-ipfs-redirects-file v0.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -79,7 +82,6 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/libp2p/go-cidranger v1.1.0 // indirect
 	github.com/libp2p/go-doh-resolver v0.5.0 // indirect
 	github.com/libp2p/go-libp2p-asn-util v0.4.1 // indirect
@@ -116,6 +118,7 @@ require (
 	github.com/ydb-platform/ydb-go-genproto v0.0.0-20241112172322-ea1f63298f77 // indirect
 	github.com/ydb-platform/ydb-go-sdk/v3 v3.108.1 // indirect
 	github.com/ziutek/mymysql v1.5.4 // indirect
+	golang.org/x/time v0.14.0 // indirect
 	gonum.org/v1/gonum v0.16.0 // indirect
 	howett.net/plist v1.0.1 // indirect
 )
@@ -211,15 +214,15 @@ require (
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/mock v0.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.45.0 // indirect
+	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/mod v0.30.0 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/term v0.37.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	golang.org/x/tools v0.38.0 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/term v0.38.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250825161204-c5933d9347a5 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250825161204-c5933d9347a5 // indirect
