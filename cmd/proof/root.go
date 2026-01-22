b3a2b61e8005d1163ad47b4f5f7ce1da526d1b68
frrist <forrest@storacha.network>
2026-01-22 15:02:43 -0800

    feat: implement repo config
    - replaces all global passing among commands

--- cmd/proof/root.go
+++ cmd/proof/root.go
@@ -4,8 +4,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var StorePathP *string
-
 var Cmd = &cobra.Command{
 	Use:   "proof",
 	Short: "Manage proofs",
--- cmd/space/root.go
+++ cmd/space/root.go
@@ -5,8 +5,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var StorePathP *string
-
 var Cmd = &cobra.Command{
 	Use:   "space",
 	Short: "Manage spaces",
