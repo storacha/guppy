d6662f46edd4a7418d769b43a13571b2a6a05e30
Petra Jaros <peeja@peeja.com>
2026-02-06 17:21:28 -0500

    refactor: Restore lost SQL comments

--- pkg/preparation/sqlrepo/migrations/20251219063531_indexes_listed_in_db.sql
+++ pkg/preparation/sqlrepo/migrations/20251219063531_indexes_listed_in_db.sql
@@ -1,19 +1,32 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS indexes (
+  -- UUID identifying the index locally
   id BLOB PRIMARY KEY,
+  -- The upload this index belongs to
   upload_id BLOB NOT NULL,
+  -- The multihash digest of the completed index
+  -- If NULL, has not yet been calculated (and maybe cannot be, if still
+  -- accepting new shards)
   digest BLOB,
+  -- The piece CID of the completed index
   piece_cid BLOB,
+  -- The size of the completed index in bytes
   size INTEGER NOT NULL DEFAULT 0,
+  -- The total number of slices (nodes) across all shards in this index
   slice_count INTEGER NOT NULL DEFAULT 0,
   state TEXT NOT NULL,
+  -- The location commitment from space/blob/add
   location_inv BLOB,
+  -- The PDP accept invocation from space/blob/add 1
   pdp_accept_inv BLOB
 ) STRICT;
 
+-- The fact that a shard has been assigned to a index.
 CREATE TABLE IF NOT EXISTS shards_in_indexes (
+  -- Which shard we're talking about
   shard_id BLOB NOT NULL,
+  -- Which index this shard is in
   index_id BLOB NOT NULL,
   FOREIGN KEY (shard_id) REFERENCES shards(id) ON DELETE CASCADE,
   FOREIGN KEY (index_id) REFERENCES indexes(id) ON DELETE CASCADE,
