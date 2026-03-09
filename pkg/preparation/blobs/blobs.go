be44cd6a20e635320115cad7ec6c188ee4e0bd3c
Petra Jaros <peeja@peeja.com>
2026-03-09 15:01:03 -0400

    refactor: Less complex error handling

--- pkg/preparation/blobs/blobs.go
+++ pkg/preparation/blobs/blobs.go
@@ -209,16 +209,11 @@ func (a API) addNodeToDigestState(ctx context.Context, shard *model.Shard, node 
 	}, nil
 }
 
-func (a API) updatedShardHashState(ctx context.Context, shard *model.Shard) (hRet *shardHashState, errRet error) {
+func (a API) updatedShardHashState(ctx context.Context, shard *model.Shard) (*shardHashState, error) {
 	h, err := fromShard(shard)
 	if err != nil {
 		return nil, fmt.Errorf("getting shard %s hasher: %w", shard.ID(), err)
 	}
-	defer func() {
-		if errRet != nil {
-			h.reset()
-		}
-	}()
 
 	if shard.DigestStateUpTo() < shard.Size() {
 		err := a.fastWriteShard(ctx, shard.ID(), shard.DigestStateUpTo(), h)
